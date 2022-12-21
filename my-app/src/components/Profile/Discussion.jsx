50aaa122f688ba53c54edccce46babce924aca15
Onufrienko.D <dmytroonufrienko@gmail.com>
2022-12-21 02:08:28 +0200

    Lesson 16 Add props to component "Comment" Create 4 comments using the props Download 2 images, but don't use it

--- my-app/src/components/Profile/Discussion.jsx
+++ my-app/src/components/Profile/Discussion.jsx
@@ -7,12 +7,12 @@ const Discussion = () => {
             <div className={classes.content}>
                 <div className={classes.content_item}>
                     <img src="https://ih1.redbubble.net/image.1137167192.4552/pp,840x830-pad,1000x1000,f8f8f8.jpg"
-                         id={classes.ava}/>
+                         id={classes.ava} alt={'avatar'}/>
                 </div>
                 <div className={classes.content_item}>
                     <p className={classes.line}>Description</p>
                 </div>
-                <Theme/>
+                <Theme />
             </div>
         </div>
     )
