1e042426ee9923973bcad95682271df569fe184a
Onufrienko.D <dmytroonufrienko@gmail.com>
2022-12-24 20:12:38 +0200

    lesson 25 Dynamically add components by map function in the "Comment" and "Messages"

--- my-app/src/components/Social/Messages/Messages.jsx
+++ my-app/src/components/Social/Messages/Messages.jsx
@@ -9,23 +9,14 @@ let users = [
     {"id": 4, "name": "girl3", "img": process.env.PUBLIC_URL + 'avatars/girl3.jpg'},
 ]
 
+let users_avatars = users
+    .map(user => (<Mess_Nav img={user.img} name={user.name} id={user.id}/>))
+
 const Messages = () => {
     return (
         <div className={classes.parent}>
             <div className={classes.mess_ava}>
-                <Mess_Nav
-                    img={users[0].img}
-                    name={users[0].name}
-                />
-                <Mess_Nav
-                    img={users[1].img}
-                    name={users[1].name}/>
-                <Mess_Nav
-                    img={users[2].img}
-                    name={users[2].name}/>
-                <Mess_Nav
-                    img={users[3].img}
-                    name={users[3].name}/>
+                {users_avatars}
             </div>
             <div className={classes.dialog}>
                 <Dialog/>
